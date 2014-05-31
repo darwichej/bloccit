2ef2af20a8af61247ccc597b7154ebc7ce65925d
Jad Darwiche <darwichej@github.com>
2014-05-31 20:04:56 +0300

    Implemented favoriting

---
+++ app/policies/favorite_policy.rb
@@ -0,0 +1,7 @@
+class FavoritePolicy < ApplicationPolicy
+  class Scope < Struct.new(:user, :scope)
+    def resolve
+      scope
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,11 @@ Bloccit::Application.routes.draw do
   resources :users, only: [:update]
   
   resources :topics do
-    resources :posts do
-      resources :comments
+    resources :posts, except: [:index] do
+      resources :comments, only: [:create, :destroy]
       get '/up-vote' => 'votes#up_vote', as: :up_vote
       get '/down-vote' => 'votes#down_vote', as: :down_vote
+      resources :favorites, only: [:create, :destroy]
     end
   end
 
